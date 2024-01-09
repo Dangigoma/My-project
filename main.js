369397b414522a7c0b5e1257839747f48ac9f05d
Goma Dangi <darshandristi1@gmail.com>
2024-01-10 10:23:53 +1100

    Third Commit Completed

---
+++ main.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const button = document.getElementById('changeColorBtn');
+    const container = document.querySelector('.container');
+    button.addEventListener('click', function(){
+        const randomColor = getRandomColor();
+        container.style.backgroundColor = randomColor;
+        
+    });
+    function getRandomColor(){
+        const letters = '123456789ABCDF';
+        let color = '#';
+        for(let i =0; i <6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+
+});
